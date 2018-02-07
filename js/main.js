91b6e903d2f3d9ea86d2d20a80bd07205d8b7f51
Daniel <wdstott@hotmail.co.uk>
2018-02-07 16:00:39 +0000

    Added contact information. Changed navbar logo, also randomly changes colour.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+(function(){
+
+//Changes the navbar titles colour
+$("#nav-text").removeClass("text-dark").addClass(function(){
+    return ["text-primary", "text-danger", "text-success", "text-warning", "text-white"][Math.floor(Math.random() * 5) ];
+});
+}());
