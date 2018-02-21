b398a4b86e434e28d692a57af03dbe95e835b215
Daniel <wdstott@hotmail.co.uk>
2018-02-21 17:20:43 +0000

    Made page more relative. Added image carousel.

--- js/main.js
+++ js/main.js
@@ -4,4 +4,6 @@
 $("#nav-text").removeClass("text-dark").addClass(function(){
     return ["text-primary", "text-danger", "text-success", "text-warning", "text-white"][Math.floor(Math.random() * 5) ];
 });
+
+
 }());
